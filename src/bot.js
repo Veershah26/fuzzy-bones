26f476b5e1991eb566b7a9388820bbc59e258318
nimit2801 <mnimitsavant@outlook.com>
2021-06-05 02:47:41 +0530

    junk folder made and revamp started

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,24 @@
+// Community Minimilastic Discord Bot
+
+// 1. Moderation KICK/BAN/WARN
+// 2. React to get roles
+// 3. DashBoard For the Bot
+// 4. Level Increase
+// 5. Language Control (Block Abusive Texts)
+
+// Import and declaration :D
 'use strict';
 require('dotenv').config();
+const { Client } = require('discord.js');
 
+// Creating Objects
+const client = new Client({
+  partials: ['MESSAGE', 'REACTION'],
+});
+
+// declaring for the bot
+let PREFIX = '$';
+let MODPREFIX = 'MD$';
 client.on('ready', () => {
   const servers = client.guilds.cache.size;
   const users = client.users.cache.size;
@@ -10,44 +28,34 @@ client.on('ready', () => {
   );
 
   client.user.setActivity(`with unicorns!`, {
-    type: 'SINGING',
+    type: 'PLAYING',
   });
 });
 
-const makeGitHubCall = async (args, message) => {
-  await axios
-    .get(`https://api.github.com/users/${args[0]}`)
-    .then((response) => {
-      let data = response.data;
-      console.log(data);
-      const {
-        login,
-        avatar_url,
-        html_url,
-        url,
-        name,
-        followers,
-        following,
-        public_repos,
-        public_gists,
-        create_at,
-        updated_at,
-      } = data;
-      const messageGitHubEmbed = new MessageEmbed()
-        .setTitle(`GitHub Profile`)
-        .setURL(html_url)
-        .setImage(avatar_url)
-        .addField('Username', login)
-        .addField('Name', name)
-        .addField('Repos', public_repos)
-        .addField('Following', following)
-        .addField('Followers', followers)
-        .addField('Follow me here', html_url)
-        .setFooter('GitHub Stats by Fuzzy Bones')
-        .setTimestamp();
-      message.channel.send(messageGitHubEmbed);
-    })
-    .catch((e) => {
-      console.log(`ERROR: ${e}`);
-    });
+const sep = (message) => {
+  let CMD_NAME, args;
+  return ([CMD_NAME, ...args] = message.content
+    .trim()
+    .substring(PREFIX.length)
+    .split(/\s+/));
 };
+
+client.on('message', async (message) => {
+  if (message.author.bot === true) return;
+  if (message.content.startsWith(PREFIX)) {
+    let [CMD_NAME, ...args] = sep(message);
+    // console.log(CMD_NAME, args[1]);
+  }
+  if (
+    message.content.startsWith(MODPREFIX) &&
+    message.member.hasPermission('ADMINISTRATOR')
+  ) {
+    try {
+      // switch ;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+client.login(process.env.DJSTOKEN);
